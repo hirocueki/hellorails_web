d5d0bcdb580dba9bee579f8d6043cc04f1f1dfd3
Hiroshi Ueki <hirocueki@gmail.com>
2019-03-19 15:10:45 +0900

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_19_044318) do
+ActiveRecord::Schema.define(version: 2019_03_19_060846) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
